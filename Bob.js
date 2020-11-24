960dff28047f1fce5a99164b5c0f03e3c0504af1
milonee608 <70271042+milonee608@users.noreply.github.com>
2020-11-24 22:48:43 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,22 @@
+class Bob{
+    constructor(x,y,radius){
+        var options={
+            isStatic: false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+
+       this.bob=Matter.Bodies.circle(x,y,radius,options);
+	    World.add(world,this.bob);
+    
+    }
+
+display(){
+    ellipseMode(RADIUS);
+    fill("blue")
+    ellipse(this.bob.position.x,this.bob.position.y,20,20);
+}
+    
+}
+  
---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint=Matter.Constraint;
+
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5,rope1,rope2,rope3,rope4,rope5,roof;
+var bobDiameter;
+
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+  bobObject1=new Bob(320,400,5);
+	bobObject2=new Bob(360,400,5);
+	bobObject3=new Bob(400,400,5);
+	bobObject4=new Bob(440,400,5);
+	bobObject5=new Bob(480,400,5);
+  roof= new Roof(400,200,200,20);
+	rope1=new Rope(bobObject1.body,roof.body,-bobDiameter*4,0);
+	rope2=new Rope(bobObject2.body,roof.body,-bobDiameter*3.5,0);
+	rope3=new Rope(bobObject3.body,roof.body,-bobDiameter*3,0);
+	rope4=new Rope(bobObject4.body,roof.body,-bobDiameter*3.5,0);
+	rope5=new Rope(bobObject5.body,roof.body,-bobDiameter*4.,0);
+
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  Engine.update(engine);
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  roof.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  drawSprites();
+ 
+}
+
+
+
